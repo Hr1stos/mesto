b7467094e11bf72e56c0a23512f381b860ce7223
Naumkin Ilya <naumkinilya@yandex.ru>
2023-03-19 09:48:45 +0400

    fix: исправть ошбки ревьюера

--- scripts/index.js
+++ scripts/index.js
@@ -2,33 +2,31 @@ const editButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileOpsane = document.querySelector('.profile__opsane');
 const container = document.querySelector('.popup');
-const saveButton = container.querySelector('.popup__save-button');
 const closeButton = container.querySelector('.popup__close-button');
 const popupInfo = container.querySelector('.popup__form');
 const nameImput = container.querySelector('.popup__input_type_name')
 const opsaneImput = container.querySelector('.popup__input_type_opsane')
 
-
-editButton.addEventListener('click', function() {
-	container.classList.add('popup_open');
+function openPopup() {
+	container.classList.add('popup_opened');
 
 	nameImput.value = profileName.textContent;
 	opsaneImput.value = profileOpsane.textContent;
-})
-
-function closePopup() {
-	container.classList.remove('popup_open');
 }
 
-saveButton.addEventListener('click', closePopup);
+function closePopup() {
+	container.classList.remove('popup_opened');
+}
+
+editButton.addEventListener('click', openPopup)
 
 closeButton.addEventListener('click', closePopup);
 
 popupInfo.addEventListener('submit', function(event) {
 	event.preventDefault();
 
-	profileName.innerHTML = nameImput.value;
-	profileOpsane.innerHTML = opsaneImput.value;
+	profileName.textContent = nameImput.value;
+	profileOpsane.textContent = opsaneImput.value;
 
-	saveButton.addEventListener('click', closePopup);
+	closePopup();
 })
