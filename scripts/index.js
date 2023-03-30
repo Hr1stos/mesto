87c3358c7dedaaec2697bb4ea680988eb34bc5ee
Naumkin Ilya <naumkinilya@yandex.ru>
2023-03-30 16:44:22 +0400

    Сделать удалене карточки, активные лайки

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,11 @@
 const initialCards = [
 	{
 		name: 'Infiniti',
-		link: 'https://images.unsplash.com/photo-1661615041341-ecde4f4f5cf2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+		link: 'https://images.unsplash.com/photo-1661615342977-bb4378751028?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDV8fGluZmluaXRpfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60'
 	},
 	{
 		name: 'Audi',
-		link: 'https://images.unsplash.com/photo-1615715070496-d85daab3618d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTZ8fGF1ZGklMjBhNnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+		link: 'https://images.unsplash.com/photo-1669265178536-378625854dbd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8ODF8fGF1ZGklMjBhN3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
 	},
 	{
 		name: 'Mercedes-Benz',
@@ -13,15 +13,15 @@ const initialCards = [
 	},
 	{
 		name: 'Challenger',
-		link: 'https://images.unsplash.com/photo-1614220683044-58f9e9548df6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80'
-	},
-	{
-		name: 'McLaren',
-		link: 'https://images.unsplash.com/photo-1621615541963-49e6f4612daa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+		link: 'https://images.unsplash.com/photo-1614220683044-58f9e9548df6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Q2hhbGxlbmdlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
 	},
 	{
 		name: 'BMW',
 		link: 'https://images.unsplash.com/photo-1556448851-9359658faa54?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80'
+	},
+	{
+		name: 'McLaren',
+		link: 'https://images.unsplash.com/photo-1621615541963-49e6f4612daa?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
 	}
 ];
 
@@ -30,21 +30,16 @@ const editButton = document.querySelector('.profile__edit-button');
 const profileName = document.querySelector('.profile__name');
 const profileOpsane = document.querySelector('.profile__opsane');
 const templateCards = document.querySelector('.template_cards').content;
-const templatePopup = document.querySelector('.template_popup').content;
-const popupContainer = templatePopup.querySelector('.popup');
-const closeButton = templatePopup.querySelector('.popup__close-button');
-const popupInfo = templatePopup.querySelector('.popup__form');
-const nameImput = templatePopup.querySelector('.popup__input_type_name');
-const opsaneImput = templatePopup.querySelector('.popup__input_type_opsane');
+const popupContainer = document.querySelector('.popup');
+const closeButton = popupContainer.querySelector('.popup__close-button');
+const popupInfo = popupContainer.querySelector('.popup__form');
+const nameImput = popupContainer.querySelector('.popup__input_type_name');
+const opsaneImput = popupContainer.querySelector('.popup__input_type_opsane');
 const cards = document.querySelector('.cards__container');
+//const likeButton = templateCards.querySelector('.cards__button_like');
 
-//console.log(renderPopup)
+//console.log(likeButton)
 
-
-function renderPopup (item) {
-	const htmlPopup = templatePopup.cloneNode(true);
-	pageContainer.append(popupContainer);
-}
 
 function openPopup() {
 	popupContainer.classList.add('popup_opened');
@@ -61,7 +56,8 @@ editButton.addEventListener('click', openPopup);
 
 closeButton.addEventListener('click', closePopup);
 
-popupInfo.addEventListener('submit', function(event) {
+
+popupInfo.addEventListener('submit', function (event) {
 	event.preventDefault();
 
 	profileName.textContent = nameImput.value;
@@ -83,7 +79,24 @@ function renderItem (item) {
 	htmlElement.querySelector('.cards__image').src = item.link;
 	htmlElement.querySelector('.cards__image').alt = item.name;
 	htmlElement.querySelector('.cards__title').textContent = item.name;
+	setEventListener(htmlElement);
 	cards.append(htmlElement);
+}
+
+function setEventListener(htmlElement) {
+	htmlElement.querySelector('.cards__button_delete').addEventListener('click', handleDelete);
+	htmlElement.querySelector('.cards__button_like').addEventListener('click', handleLike);
+}
+
+//Удаление карточки
+function handleDelete (event) {
+	const card = event.target.closest('.cards__item');
+	card.remove();
+}
+
+//Нажатие лайка
+function handleLike (event) {
+	event.target.classList.toggle('cards__button_like_active');
 }
 
 //Добавление новой карточки на страницу
