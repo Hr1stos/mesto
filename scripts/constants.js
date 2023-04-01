2a66559b3d87c30ca20028f436244b1097084cc8
Naumkin Ilya <naumkinilya@yandex.ru>
2023-04-01 20:30:24 +0400

    Сделать открытие popupImage

--- scripts/constants.js
+++ scripts/constants.js
@@ -1,27 +1,27 @@
 const initialCards = [
 	{
-		name: 'Россия',
-		link: 'https://images.unsplash.com/photo-1520106212299-d99c443e4568?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
+		name: 'Канада',
+		link: 'https://images.unsplash.com/photo-1502003148287-a82ef80a6abc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNhbmFkYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
 	},
 	{
-		name: 'Грузия',
-		link: 'https://images.unsplash.com/photo-1568490891363-466fe21bede2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=290&q=80'
-	},
-	{
-		name: 'США',
-		link: 'https://images.unsplash.com/photo-1580752300984-a479ef2f97dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fHVzYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
-	},
-	{
-		name: 'Дубай',
-		link: 'https://images.unsplash.com/photo-1582672060674-bc2bd808a8b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80'
+		name: 'Аргентина',
+		link: 'https://images.unsplash.com/photo-1610680224983-f9759ce81c7a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjJ8fGFyZ2VudGluYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
 	},
 	{
 		name: 'Швейцария',
 		link: 'https://images.unsplash.com/photo-1675503989795-a5661ebd1536?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjR8fHNjaHdlaXplcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
 	},
 	{
-		name: 'Канада',
-		link: 'https://images.unsplash.com/photo-1502003148287-a82ef80a6abc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTF8fGNhbmFkYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+		name: 'США',
+		link: 'https://images.unsplash.com/photo-1580752300984-a479ef2f97dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fHVzYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
+	},
+	{
+		name: 'Грузия',
+		link: 'https://images.unsplash.com/photo-1568490891363-466fe21bede2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=290&q=80'
+	},
+	{
+		name: 'Россия',
+		link: 'https://images.unsplash.com/photo-1520106212299-d99c443e4568?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80'
 	}
 ];
 
--- scripts/index.js
+++ scripts/index.js
@@ -1,55 +1,50 @@
 import { initialCards } from "./constants.js";
 
-const cardsTemplate = document.querySelector('.template_cards');
-const cardsContainer = document.querySelector('.cards__container');
-const popupEdit = document.querySelector('.popup_edit');
-const popupAdd = document.querySelector('.popup_add');
-const editButtonPopup = document.querySelector('.profile__edit-button');
-const addButtonPopup = document.querySelector('.profile__add-button');
-const closeButtonPopupEdit = popupEdit.querySelector('.popup__close-button_edit');
-const closeButtonPopupAdd = popupAdd.querySelector('.popup__close-button_add');
-const inputNamePopupEdit = popupEdit.querySelector('.popup__input_type_name');
-const inputOpsanePopupEdit = popupEdit.querySelector('.popup__input_type_opsane');
-const inputNamePopupAdd = popupAdd.querySelector('.popup__input_type_name');
-const inputLinkPopupAdd = popupAdd.querySelector('.popup__input_type_link');
-const profileName = document.querySelector('.profile__name');
-const profileOpsane = document.querySelector('.profile__opsane');
-const formPopupEdit = popupEdit.querySelector('.popup__form');
-const formPopupAdd = popupAdd.querySelector('.popup__form');
-
-//Создание карточек на страницу, лайки, удаление карточки
+//****Создание карточек на страницу, лайки, удаление карточки, открытие PopupImage****
+const cardsTemplate = document.querySelector('.template-cards');
 function createCardElement (cardData) {
 	const cardElement = cardsTemplate.content
 		.querySelector('.cards__item')
 		.cloneNode(true);
 
-	const linkCard = cardElement.querySelector('.cards__image');
-	const altCard = cardElement.querySelector('.cards__image');
+	const imageCard = cardElement.querySelector('.cards__image');
 	const nameCerd = cardElement.querySelector('.cards__title');
 
-	linkCard.src = cardData.link;
-	altCard.alt = cardData.name;
+	imageCard.src = cardData.link;
+	imageCard.alt = cardData.name;
 	nameCerd.textContent = cardData.name;
 
 	const likeButton = cardElement.querySelector('.cards__button_like');
-	const deleteButton = cardElement.querySelector('.cards__button_delete');
-
 	function handleLike() {
-		likeButton.classList.toggle('cards__button_like_active')
+		likeButton.classList.toggle('cards__button_like_active');
 	};
-
 	likeButton.addEventListener('click', handleLike);
 
+	const deleteButton = cardElement.querySelector('.cards__button_delete');
 	function handleDelete() {
 		cardElement.remove();
 	};
-
 	deleteButton.addEventListener('click', handleDelete);
-	
+
+	imageCard.addEventListener('click', () => {
+		openPopup(popupImage);
+		const image = popupImage.querySelector('.popup__img');
+		const caption = popupImage.querySelector('.popup__caption');
+		const nameCerd = document.querySelector('.cards__title');
+
+		image.src = imageCard.src;
+		image.alt = imageCard.alt;
+		caption.textContent = nameCerd.textContent;
+	});
+
 	return cardElement
 };
 
-//Загрузка карточек на странцу
+
+
+//****Загрузка карточек на странцу****
+const cardsContainer = document.querySelector('.cards__container');
+
 function renderCardElement(cardElement) {
 	cardsContainer.prepend(cardElement);
 };
@@ -59,39 +54,61 @@ initialCards.forEach(function (cards) {
 	renderCardElement(item);
 });
 
-//Открыть popup
+
+
+//****Открыть popup****
 function openPopup(popup) {
 	popup.classList.add('popup_opened');
 };
 
+const editButtonPopup = document.querySelector('.profile__edit-button');
+const popupEdit = document.querySelector('.popup_edit');
+const profileName = document.querySelector('.profile__name');
+const profileOpsane = document.querySelector('.profile__opsane');
 editButtonPopup.addEventListener('click', () => {
 	openPopup(popupEdit);
-
 	inputNamePopupEdit.value = profileName.textContent;
 	inputOpsanePopupEdit.value = profileOpsane.textContent;
 });
 
+const addButtonPopup = document.querySelector('.profile__add-button');
+const popupAdd = document.querySelector('.popup_add');
 addButtonPopup.addEventListener('click', () => {
 	openPopup(popupAdd);
-
 	inputNamePopupAdd.value = '';
 	inputLinkPopupAdd.value = '';
 });
 
-//Закрыть popup
+
+
+//****Закрыть popup****
 function closePopup(popup) {
 	popup.classList.remove('popup_opened');
 };
 
+const closeButtonPopupEdit = popupEdit.querySelector('.popup__close-button_edit');
 closeButtonPopupEdit.addEventListener('click', () => {
 	closePopup(popupEdit);
 });
 
+const closeButtonPopupAdd = popupAdd.querySelector('.popup__close-button_add');
 closeButtonPopupAdd.addEventListener('click', () => {
 	closePopup(popupAdd);
 });
 
-//Сохранть изменения popupEdit
+const popupImage = document.querySelector('.popup_image');
+const closeButtonPopupImage = popupImage.querySelector('.popup__close-button_image');
+closeButtonPopupImage.addEventListener('click', () => {
+	closePopup(popupImage);
+});
+
+
+
+//****Сохранить изменения popupEdit****
+const formPopupEdit = popupEdit.querySelector('.popup__form');
+const inputNamePopupEdit = popupEdit.querySelector('.popup__input_type_name');
+const inputOpsanePopupEdit = popupEdit.querySelector('.popup__input_type_opsane');
+
 formPopupEdit.addEventListener('submit', (event) => {
 	event.preventDefault();
 
@@ -101,7 +118,13 @@ formPopupEdit.addEventListener('submit', (event) => {
 	closePopup(popupEdit);
 });
 
-//Сохранить измененя popupAdd
+
+
+//****Сохранить измененя popupAdd****
+const formPopupAdd = popupAdd.querySelector('.popup__form');
+const inputNamePopupAdd = popupAdd.querySelector('.popup__input_type_name');
+const inputLinkPopupAdd = popupAdd.querySelector('.popup__input_type_link');
+
 formPopupAdd.addEventListener('submit', (event) => {
 	event.preventDefault();
 
@@ -116,3 +139,5 @@ formPopupAdd.addEventListener('submit', (event) => {
 	renderCardElement(createCardElement(cardNew));
 	closePopup(popupAdd);
 });
+
+
